e66623381cd304b3e28536f927cb0ea82fdfbcc6
tamat <libastral.so@yandex.ru>
2020-11-24 18:32:51 +0700

    #7 Review fixes 2

--- storage-service/src/main/kotlin/com/icerockdev/service/storage/preview/PreviewScaleMethod.kt
+++ storage-service/src/main/kotlin/com/icerockdev/service/storage/preview/PreviewScaleMethod.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package com.icerockdev.service.storage.preview
 
 import com.sksamuel.scrimage.ScaleMethod
@@ -9,7 +13,7 @@ enum class PreviewScaleMethod {
     Bilinear,
     Bicubic;
 
-    fun getScale(): ScaleMethod {
+    internal fun getScale(): ScaleMethod {
         return when(this) {
             FastScale -> ScaleMethod.FastScale
             Lanczos3 -> ScaleMethod.Lanczos3
